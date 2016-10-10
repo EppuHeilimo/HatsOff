a14af3b5d761403bb5d298f2e190b93989d3cae6
H8566 <H8566@ELEK-431-WS-17>
2016-10-10 15:50:21 +0300

    asdsad

--- MoveShape/MoveShape.cs
+++ MoveShape/MoveShape.cs
@@ -2,22 +2,50 @@
 using System.Threading;
 using Microsoft.AspNet.SignalR;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace MoveShape
 {
+    public class Server
+    {
+        private Broadcaster _broadcaster;
+        private readonly static Lazy<Server> _server = new Lazy<Server>(() => new Server());
+        private List<RemotePlayer> connectedPlayers;
+        public Server()
+        {
+            connectedPlayers = new List<RemotePlayer>();
+        }
+
+        public void NewConnection(string connectionID)
+        {
+            connectedPlayers.Add(new RemotePlayer(connectionID));
+            _broadcaster.AddPlayer();
+        }
+
+        public static Server ServerInstance
+        {
+            get
+            {
+                return _server.Value;
+            }
+        }
+    }
+
     public class Broadcaster
     {
         private readonly static Lazy<Broadcaster> _instance =
             new Lazy<Broadcaster>(() => new Broadcaster());
         // We're going to broadcast to all clients a maximum of 25 times per second
         private readonly TimeSpan BroadcastInterval =
-            TimeSpan.FromMilliseconds(40);
+            TimeSpan.FromMilliseconds(16);
         private readonly IHubContext _hubContext;
+
         private Timer _broadcastLoop;
         private ShapeModel _model;
         private ShapeModel _model2;
         private int _shapeID;
         private bool _modelUpdated;
+        private bool _playerJoined;
         public Broadcaster()
         {
             // Save our hub context so we can easily use it 
@@ -25,14 +53,15 @@ namespace MoveShape
             _hubContext = GlobalHost.ConnectionManager.GetHubContext<MoveShapeHub>();
             _model = new ShapeModel();
             _modelUpdated = false;
+            _playerJoined = false;
             // Start the broadcast loop
             _broadcastLoop = new Timer(
-                BroadcastShape,
+                Broadcast,
                 null,
                 BroadcastInterval,
                 BroadcastInterval);
         }
-        public void BroadcastShape(object state)
+        public void Broadcast(object state)
         {
             // No need to send anything if our model hasn't changed
             if (_modelUpdated)
@@ -42,6 +71,11 @@ namespace MoveShape
                 _hubContext.Clients.AllExcept(_model.LastUpdatedBy).updateShape(_model, _shapeID);
                 _modelUpdated = false;
             }
+            if(_playerJoined)
+            {
+                _hubContext.Clients.AllExcept(_model.LastUpdatedBy).addPlayer();
+                _playerJoined = false;
+            }
 
         }
         public void UpdateShape(ShapeModel clientModel, int shapeID)
@@ -50,6 +84,12 @@ namespace MoveShape
             _model = clientModel;
             _modelUpdated = true;
         }
+
+        public void AddPlayer()
+        {
+            _playerJoined = true;
+        }
+
         public static Broadcaster Instance
         {
             get
@@ -63,9 +103,12 @@ namespace MoveShape
     {
         // Is set via the constructor on each creation
         private Broadcaster _broadcaster;
+        private Server _server;
         public MoveShapeHub()
             : this(Broadcaster.Instance)
         {
+            _server = Server.ServerInstance;
+            _server.NewConnection(Context.ConnectionId);
         }
         public MoveShapeHub(Broadcaster broadcaster)
         {
@@ -89,7 +132,19 @@ namespace MoveShape
         // We don't want the client to get the "LastUpdatedBy" property
         [JsonIgnore]
         public string LastUpdatedBy { get; set; }
-
     }
 
+    public class RemotePlayer
+    {
+        ShapeModel _playerShape;
+        string _connectionID;
+        public RemotePlayer(string connectionID)
+        {
+            _connectionID = connectionID;
+            _playerShape = new ShapeModel();
+            _playerShape.Left = 0;
+            _playerShape.Top = 0;
+            _playerShape.LastUpdatedBy = connectionID;
+        }
+    }
 }
