082fea0ae83ef4133f8b371af9b4c04f2a0f6101
MasterTaffer <odestrzo@gmail.com>
2016-10-31 14:59:50 +0200

    Add collision test(s)

---
+++ MoveShape/Collision.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Hatsoff
+{
+    static class Collision
+    {
+        
+        static bool TestCircleCollision(Vec2 c1, double r1, Vec2 c2, double r2)
+        {
+            Vec2 delta = c2 - c1;
+
+            if (delta.x * delta.x + delta.y * delta.y > (r1 + r2) * (r1 + r2))
+                return true;
+            return false;
+        }
+
+        
+    }
+}
